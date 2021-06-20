ec1bbc4a39343069cbd04d301b5e090b00433106
manojap <manojap@outlook.com>
2021-06-20 11:25:25 +0530

    .

---
+++ dist/_nuxt/static/1624167236/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624167236",layout:"default",error:a,state:{blog:{posts:[{}],isLoaded:false},config:{siteConfig:{title:"CodeHat",logo:void 0},isLoaded:b}},serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,true,"\u002F"));
